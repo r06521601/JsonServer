79d9b89a315333df38fff767f9367d2475203352
royhuang <r06521601@ntu.edu.tw>
2019-11-25 21:49:51 +0800

    api done

--- index.js
+++ index.js
@@ -7,13 +7,55 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+//ex: http://localhost:5001/database
 app.get('/database', function (req, res) {
     var contents = fs.readFileSync("db.json");
   
     var jsonContent = JSON.parse(contents);
+    console.log(typeof(jsonContent.inspection))
+
   
     res.send(jsonContent);
     });
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+
+
+//ex: http://localhost:5001/insert?id={value}
+app.get('/insert', (req, res) => {
+    var contents = fs.readFileSync("db.json");
+
+    var jsonContent = JSON.parse(contents);
+
+    var tempData = new Array()
+
+    for ( var index=0; index<jsonContent.inspection.length; index++ ) {
+        tempData.push(jsonContent.inspection[index])
+    }
+
+    //push data
+    tempData.push({"id": getParameterByName("id", req.url)})
+
+
+    //store
+    jsonContent.inspection = tempData
+    
+    let data = JSON.stringify(jsonContent);
+
+    console.log(data)
+
+    fs.writeFileSync('db.json', data);
+
+    res.send("done");
+    })
 
 const PORT = process.env.PORT || 5001;
 
