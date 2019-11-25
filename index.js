5258faf9fcbd1d8328b043de3736c9b67b1eb234
royhuang <r06521601@ntu.edu.tw>
2019-11-25 19:58:56 +0800

    database api

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const fs = require('fs');
+
 var app = express();
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
+
+app.get('/database', function (req, res) {
+    var contents = fs.readFileSync("db.json");
+  
+    var jsonContent = JSON.parse(contents);
+  
+    res.send(jsonContent);
+    });
+
 const PORT = process.env.PORT || 5001;
 
 var server = app.listen(PORT, function () {
